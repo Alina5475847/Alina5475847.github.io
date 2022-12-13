b76c2c3d9fe8578b4e0c8f4bed3d25840aab9232
Alina5475847 <94994296+Alina5475847@users.noreply.github.com>
2022-12-13 17:14:27 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.page').slick({
+        vertical: true,
+        dots: true,
+        arrows: false,
+        swipe: false,
+        slidesToShow: 1
+    });
+});
