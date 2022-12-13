0c9109aa02d1523bc7768c4701096a1201d2daef
Alina5475847 <94994296+Alina5475847@users.noreply.github.com>
2022-12-13 18:33:17 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.page').slick({
-        vertical: true,
-        dots: true,
-        arrows: false,
-        swipe: false,
-        slidesToShow: 1
-    });
-});
