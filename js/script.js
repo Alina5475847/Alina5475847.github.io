dc7d0c70fde59aae220487d06df57a2400ad4be8
Alina5475847 <94994296+Alina5475847@users.noreply.github.com>
2022-12-17 09:24:23 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.header__data-item-btn').addEventListener('click', function() {
-    document.querySelector('.header__cab').classList.toggle('header__cab--visible');
-    this.classList.toggle('header__data-item-btn--active');
-});
